cc37e1d6bf8dec451e338d90ddc3578be712c006
chamara321 <chamarasadakalum68@gmail.com>
2025-05-16 05:00:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://t4.ftcdn.net/jpg/05/68/98/19/360_F_568981938_R87rarBphgiNJ4Kiqj0IQM6aAFgA5VWX.jpg"
+ALIVE_MSG: process.env.ALIVE_MSG || "හාලෝ මම ඩ්‍රැකියුලා I am alive new".
 };
